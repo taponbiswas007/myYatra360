3c81aa5234df8ed0061b07806d32c3da162a4763
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-03-30 15:51:58 +0600

    best recommended place updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,4 +90,49 @@ $(document).ready(function () {
             // instead of a settings object
         ]
     });
+
+
+
+    // bestrecommendedSlider slider area
+    $('.bestrecommendedSlider').slick({
+        prevArrow: $('.custom-prev'),
+        nextArrow: $('.custom-next'),
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        centerPadding: '20px',
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
 });
