d6e4615ef2cb2740a72949dce930949e03959b5b
taponbiswas007 <taponbiswassdp@gmail.com>
2025-04-30 20:16:51 +0600

    fdgdfg

--- assets/js/exploration.js
+++ assets/js/exploration.js
@@ -3,6 +3,11 @@ $(document).ready(function () {
         $(".placeTablist li button").removeClass("active");
         $(this).addClass('active');
     });
+    $(".subplaceList button").click(function(){
+        $(".subplaceList button").removeClass("active");
+        $(this).addClass('active');
+        
+    });
 
 
     $('.explorarContent').hide(); // Hide all by default
@@ -21,19 +26,30 @@ $(document).ready(function () {
     $('.mallikarjunaBtn').click(function () {
         changeTab('mallikarjunaContent');
     });
-    $('.mahakaleshwarBtn').click(function () {
-        changeTab('cancelledStatus');
-    });
-    $('.omkareshwarBtn').click(function () {
-        changeTab('rejectedStatus');
-    });
-    $('.kedarnathBtn').click(function () {
-        changeTab('escalatedStatus');
-    });
-    $('.bhimashankarBtn').click(function () {
-        changeTab('escalatedStatus');
-    });
-    $('.bhimashankarBtn').click(function () {
-        changeTab('escalatedStatus');
-    });
+   
+
+
+    // sub place list changer
+    // $('.subplacelistitems').hide(); // Hide all by default
+    // $('#jyotirlinga1subitem').show().addClass('subplacelistActive'); // Show default one
+
+    // function changeTab(target) {
+    //     $('.subplacelistitems.subplacelistActive').fadeOut(300, function () {
+    //         $(this).removeClass('subplacelistActive');
+    //         $('#' + target).fadeIn(300).addClass('subplacelistActive');
+    //     });
+    // }
+
+    // $('.jyotirlinga1btn').click(function () {
+    //     changeTab('jyotirlinga1subitem');
+    // });
+    // $('.jyotirlinga2btn').click(function () {
+    //     changeTab('jyotirlinga2subitem');
+    // });
+    // $('.shaktiPithbtn').click(function () {
+    //     changeTab('shaktipitharea');
+    // });
+
+  
+   
 })
