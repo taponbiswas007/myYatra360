c30e5c1e987529727b35378b1d01eee55ecb47f7
taponbiswas007 <taponbiswassdp@gmail.com>
2025-04-30 18:43:05 +0600

    dfsdf

--- assets/js/exploration.js
+++ assets/js/exploration.js
@@ -2,5 +2,38 @@ $(document).ready(function () {
     $(".placeTablist li button").click(function(){
         $(".placeTablist li button").removeClass("active");
         $(this).addClass('active');
-    })
+    });
+
+
+    $('.explorarContent').hide(); // Hide all by default
+    $('#somnathContent').show().addClass('contentActive'); // Show default one
+
+    function changeTab(target) {
+        $('.explorarContent.contentActive').fadeOut(300, function () {
+            $(this).removeClass('contentActive');
+            $('#' + target).fadeIn(300).addClass('contentActive');
+        });
+    }
+
+    $('.somnathBtn').click(function () {
+        changeTab('somnathContent');
+    });
+    $('.mallikarjunaBtn').click(function () {
+        changeTab('mallikarjunaContent');
+    });
+    $('.mahakaleshwarBtn').click(function () {
+        changeTab('cancelledStatus');
+    });
+    $('.omkareshwarBtn').click(function () {
+        changeTab('rejectedStatus');
+    });
+    $('.kedarnathBtn').click(function () {
+        changeTab('escalatedStatus');
+    });
+    $('.bhimashankarBtn').click(function () {
+        changeTab('escalatedStatus');
+    });
+    $('.bhimashankarBtn').click(function () {
+        changeTab('escalatedStatus');
+    });
 })
