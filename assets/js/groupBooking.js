aaaa0259b88efb5d47a5ab8c7626582f03487165
taponbiswas007 <taponbiswassdp@gmail.com>
2025-05-03 16:20:36 +0600

    jgh

---
+++ assets/js/groupBooking.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('.groupBookingSlider').slick({
+        prevArrow: $('.group-prev'),
+        nextArrow: $('.group-next'),
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        centerPadding: '20px',
+        autoplay: true,
+        cssEase: "linear",
+        pushOnHover: true,
+        autoplaySpeed: 2000,
+    
+        responsive: [{
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    
+})
