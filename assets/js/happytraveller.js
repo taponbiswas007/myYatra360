e79c70f690a919b40f6011e25b7409eb44f6ad51
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2025-05-23 00:17:51 +0600

    complete

--- assets/js/happytraveller.js
+++ assets/js/happytraveller.js
@@ -1,20 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.tr_card_slide').slick({
-        
+
         dots: true,
         infinite: true,
         speed: 300,
-        slidesToShow: 5,
-        slidesToScroll: 2,
+        slidesToShow: 6,
+        slidesToScroll: 1,
         arrows: true,
         centerPadding: '20px',
         autoplay: true,
         cssEase: "linear",
         pushOnHover: true,
         autoplaySpeed: 2000,
-    
+        loop: true,
+
         responsive: [{
-                breakpoint: 1300,
+                breakpoint: 1620,
                 settings: {
                     slidesToShow: 5,
                     slidesToScroll: 1,
@@ -22,16 +23,54 @@ $(document).ready(function(){
                 }
             },
             {
-                breakpoint: 993,
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1250,
                 settings: {
                     slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1080,
+                settings: {
+                    slidesToShow: 3.5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2.5,
                     slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 578,
+                settings: {
+                    slidesToShow: 1.5,
                     slidesToScroll: 1
                 }
             },
@@ -44,5 +83,107 @@ $(document).ready(function(){
             }
         ]
     });
-    
+    $('.viewdetailsslider').slick({
+        prevArrow: $('.viewmore-prev'),
+        nextArrow: $('.viewmore-next'),
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: true,
+        centerPadding: '20px',
+        autoplay: true,
+        cssEase: "linear",
+        pushOnHover: true,
+        autoplaySpeed: 2000,
+        loop: true,
+
+        responsive: [{
+                breakpoint: 1620,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1250,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 1080,
+                settings: {
+                    slidesToShow: 3.5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2.5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 578,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.travellers_details_slide').slick({
+        prevArrow: $('.tr_d_prev'),
+        nextArrow: $('.tr_d_next'),
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        centerPadding: '20px',
+        autoplay: true,
+        cssEase: "linear",
+        pushOnHover: true,
+        autoplaySpeed: 2000,
+        loop: true,
+
+    });
+
 })
