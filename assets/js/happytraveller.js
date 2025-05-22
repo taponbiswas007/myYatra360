3a19fe216137ea710fe55d22fdb8a76dae5589e6
taponbiswas007 <taponbiswassdp@gmail.com>
2025-05-22 19:18:34 +0600

    happy traveller updated

---
+++ assets/js/happytraveller.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $('.tr_card_slide').slick({
+        
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        centerPadding: '20px',
+        autoplay: true,
+        cssEase: "linear",
+        pushOnHover: true,
+        autoplaySpeed: 2000,
+    
+        responsive: [{
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 993,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1.5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    
+})
